eae8cf98a232ee9f62e1bec3282056a00207fffb
Midhat khan <midhatkhan342@gmail.com>
2021-12-13 01:18:26 +0500

    first commit

--- tour.java
+++ tour.java
@@ -209,3 +209,221 @@ li2.addElement("NONE(0,0)");
 
 final JList<String> list2= new JList<>(li2);
 list2.setBounds(800, 300, 125, 125);
+JButton Receipt= new JButton("Generate Receipt"); //THIS BUTTON GENERATES RECEIPT
+Receipt.setBounds(600, 490, 150, 30);
+JButton b2 = new JButton("Reset");      //RESET BUTTON WILL RESET FORM
+b2.setBounds(750, 490, 150, 30);
+JButton Print = new JButton("Print");  //PRINT BUTTON WILL PRINT YOUR GENERATED RECEIPT
+Print.setBounds(900, 490, 150, 30);
+
+area2 = new JTextArea();              //AREA WHERE RECEIPT WILL BE PRINTED
+area2.setBounds(600, 540, 560, 340);
+
+
+//ADDING ALL THINGS INTO RECEIPT
+//-------------------------------
+add(l1);
+add(l2);
+add(l3);
+add(l4);
+add(l5);
+add(l6);
+add(l7);
+add(l8);
+add(l9);
+add(l10);     
+add(l11);
+add(l12);
+add(l13);
+add(l14);
+add(tf1);
+add(tf2);
+add(tf3);
+add(tf4);
+add(tf5);
+add(tf6);
+add(tf7);
+add(tf8);
+add(area1);
+add(area2);
+add(l15);
+add(rb1);
+add(rb2);
+add(rb3);
+add(rb4);
+add(r5);
+add(r6);
+add(cb);
+add(cb1);
+add(cb2);
+add(list1);
+add(list2);
+add(b);
+add(label);
+add(Receipt);
+add(b2);
+add(Print);
+
+b.addActionListener(new ActionListener() {
+
+// Method to display the data
+// entered in the text fields
+@Override
+//DEAILS WILL BE SHOWEN WHEN CLICKED ON SHOW
+public void actionPerformed(ActionEvent e){
+    String data = "";
+    if (list1.getSelectedIndex() != -1) {
+        data = "NO OF PEOPLE SELECTED :"+ list1.getSelectedValue();
+        label.setText(data);
+    }
+    if (list2.getSelectedIndex() != -1) {
+        data += " AND TRAVEL "+ "CHARGES OF: ";
+        for (Object frame :list2.getSelectedValues()) {
+            data += frame + " ";
+        }
+    }
+    label.setText(data);
+}
+}
+);
+// Reset the text fields
+b2.addActionListener(
+new ActionListener() {
+public void actionPerformed(
+ActionEvent e)
+{
+area2.setText("");
+area1.setText(" ");
+tf1.setText("");
+tf2.setText("");
+tf3.setText("");
+tf4.setText("");
+tf5.setText("");
+tf6.setText("");
+                                       
+                                       
+}
+});
+
+// Implementing the Print action
+Print.addActionListener(
+new ActionListener() {
+public void actionPerformed(ActionEvent e){
+    try {
+        area2.print();
+    }
+    catch (java.awt.print.PrinterException a) {
+        System.err.format("NoPrinter Found",a.getMessage());
+    }
+}});
+
+// Generating the receipt
+Receipt.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e){
+        area2.setText("------------------------------------"+ "-----------TOUR RECEIPT--------"+ "----------------------------------"+ "----------------------------------"+ "------------------------------\n");
+        area2.setText(area2.getText()
+                + "FIRST NAME : "
+                + tf1.getText()
+                + "\n");
+        area2.setText(area2.getText()
+                + "SECOND NAME : "
+                + tf2.getText()
+                + "\n");
+        area2.setText(area2.getText()
+                + "POST CODE : "
+                + tf3.getText()
+                + "\n");
+        area2.setText(area2.getText()
+                + "EMAIL ID : "
+                + tf4.getText()
+                + "\n");
+        area2.setText(area2.getText()
+                + "CONTACT NUMBER : "
+                + tf5.getText()
+                + "\n");
+        area2.setText(area2.getText()
+                + "NATIONALITY : "
+                + tf6.getText()
+                + "\n");
+        area2.setText(area2.getText()
+                + "DEPARTURE : "
+                + cb1.getSelectedItem()
+                + "\n");
+        area2.setText(area2.getText()
+                + "DESTINATION : "
+                + cb2.getSelectedItem()
+                + "\n");
+        area2.setText(area2.getText()
+                + "CNIC NUMBER : "
+                + tf7.getText()
+                + "\n");
+        area2.setText(area2.getText()
+                + "BOOKING DATE : "
+                + tf8.getText()
+                + "\n");
+        area2.setText(area2.getText()
+                + "NO OF PEOPLE : "
+                + cb.getSelectedItem().toString()
+                + "\n");
+        if (rb1.isSelected()) {
+            area2.setText(area2.getText() + "TAXES PAID : "+ "AIRPORT TAX PAID \n");
+        }
+        if (rb2.isSelected()) {
+            area2.setText(area2.getText()+ "TAXES PAID : "+ "AIR MILES OVER 2000 \n");
+        }
+        if (rb3.isSelected()) {area2.setText(area2.getText()+ "MORE TAXES : "+ "TRAVELING INSURANCE PAID \n");
+        }
+        if (rb4.isSelected()) {
+            area2.setText(area2.getText()+ "MORE TAXES : "+ "EXTRA LUGGAGE \n");
+        }
+        area2.setText(area2.getText()+ "PRICES : "+ list1.getSelectedValue().toString()+ "\n");
+        area2.setText(area2.getText()+ "PRICES : "+ list2.getSelectedValue().toString()+ "\n");
+        int index2 = list2.getSelectedIndex();
+        if (index2 == 0) {
+            area2.setText(area2.getText()+ "TAXES INCLUDE/DETECTED :\n"+ "TOTAL AMOUNT TO BE "+ "PAID IS 2 LAKHS \n");
+        }
+        if (index2 == 1) {
+            area2.setText(area2.getText()+ "TAXES INCLUDE/DETECTED :\n"+ "TOTAL AMOUNT TO BE "+ "PAID IS 3,34,000 \n");
+        }
+        if (index2 == 2) {
+            area2.setText(area2.getText()+ "TAXES INCLUDE/DETECTED :\n"+ "TOTAL AMOUNT TO BE PAID IS "+ "470,000 \n");
+        }
+        if (index2 == 3) {
+            area2.setText(area2.getText()+ "TAXES INCLUDE/DETECTED :\n"+ "TOTAL AMOUNT TO BE PAID "+ "IS 612,500 \n");
+        }
+        if (index2 == 4) {
+            area2.setText(area2.getText()+ "TAXES INCLUDE/DETECTED :\n"+ "TOTAL AMOUNT TO BE PAID "+ "IS 7,60,000 \n");
+        }
+        if (e.getSource() == Receipt) {
+            try {
+                FileWriter fw= new FileWriter("java.txt", true);
+                fw.write(area2.getText());
+                fw.close();
+            }
+            catch (IOException ae) {
+                System.out.println(ae);
+            }
+        }
+        JOptionPane.showMessageDialog(area2, "DATA SAVED SUCCESSFULLY");
+    };
+});
+addWindowListener(new WindowAdapter() {
+    //WONDOW WILL BE CLOSED ON EXITED
+    @Override
+    public void windowClosing(WindowEvent we){
+        System.exit(0);
+    }
+});
+setSize(1200, 1200);
+setLayout(null);
+setVisible(true);
+setBackground(Color.lightGray);
+}    
+    public static void main(String[] args){
+        A a = new A();
+        a.recipt();
+    }
+
+   
+    }
